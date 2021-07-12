ae053fb59150e0b599780d1a87a03ad16fbf6eb0
Shahbaazkyz <shahbaazkyz@gmail.com>
2021-07-12 08:22:10 +0500

    If Else Challenge

---
+++ src/solution.java
@@ -0,0 +1,31 @@
+import  java.util.*;
+
+public class solution {
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Number to Check");
+        int a = input.nextInt();
+        String ans ;
+        if (a % 2 ==1)
+        {
+            ans = "Weird";
+        }
+        else
+        {
+            if (a>2 && a<5)
+            {
+                ans = "Not Weird";
+            }
+            else if (a>5 && a<20)
+            {
+                ans = "Weird";
+            }
+            else
+            {
+                ans = "Not Weird";
+            }
+        }
+        System.out.println(ans);
+    }
+}
